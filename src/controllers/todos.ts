5051d6c89305c27dc786615ed225734520f956b1
Pedro Ulrich <79576174+ulrichheringer@users.noreply.github.com>
2024-01-03 19:11:39 -0300

    created createTodo controller

--- src/controllers/todos.ts
+++ src/controllers/todos.ts
@@ -2,10 +2,22 @@ import mongoose, { ObjectId } from "mongoose";
 import { Request, Response, NextFunction } from "express";
 import { Todo } from "../models/todo";
 
-const getTodos = async (req: Request, res: Response, next: NextFunction) => {
+const getTodos = async (req: Request, res: Response) => {
     let userId = new mongoose.Types.ObjectId(req.params.userId);
     let todos = await Todo.find({ userId }).lean();
     return res.status(200).json(todos);
 };
 
-export default { getTodos };
+const createTodo = async (req: Request, res: Response) => {
+    let todo = await Todo.create({
+        name: req.body.name,
+        description: req.body.description,
+        status: req.body.status,
+        edited: req.body.edited,
+        userId: req.body.userId,
+        projectId: req.body.projectId,
+    });
+    return res.status(201).json(todo);
+};
+
+export default { getTodos, createTodo };
